b627881f1406a3785878e252f3a2744790974826
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2019-03-27 17:24:11 -0500

    Improve type clarity
    Also cleaned up quote style consistency

--- advent.py
+++ advent.py
@@ -3,21 +3,21 @@ import sys
 import yaml
 
 if __name__ == '__main__':
-    path = sys.argv[1]  # TODO: handle index error
+    path = str(sys.argv[1])  # TODO: handle index error
     file = open(path, mode='r')  # Load file
     story_doc = yaml.safe_load(file)  # Parse file into YAML
     index = 0  # The index of the current question
     while True:
-        frame = story_doc[index]  # The actual current question
-        print(str(frame['question']))  # Print the question
+        question = dict(story_doc[index])  # The actual current question
+        print(str(question['question']))  # Print the question
         try:
-            answers = dict(frame['answers'])  # If there are answers, build them into a dictionary
+            answers = dict(question['answers'])  # If there are answers, build them into a dictionary
         except LookupError:
             break  # If there's no answer field, this is an end question
-        print("Choices: %s" % [choice_text for choice_text in answers.keys()])  # Print the answer choices
+        print('Choices: %s' % [str(choice_text) for choice_text in answers.keys()])  # Print the answer choices
         next_question_index = -1  # Because an index cannot be -1
         for i in range(0, 5):
-            choice_input = str(input("Choose an answer: "))  # Get the user input
+            choice_input = str(input('Choose an answer: '))  # Get the user input
             try:
                 next_question_index = int(answers[choice_input])  # Parse the input
             except LookupError:
@@ -27,7 +27,7 @@ if __name__ == '__main__':
                 continue
             break
         if next_question_index == -1:
-            print("You insist on being stupid? Fine. I'm done")
-            raise TimeoutError("Number of input tries exceeded")  # TODO: different error?
+            print('You insist on being stupid? Fine. I\'m done')
+            raise TimeoutError('Number of input tries exceeded')  # TODO: different error?
         else:
             index = next_question_index - 1  # Our data is 0-index
